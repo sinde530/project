72f66a38530f45a80c170b7aef857179cc0af754
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-04-26 18:36:34 +0900

    components create

--- react/dream-coding/src/App.js
+++ react/dream-coding/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Habit from './components/Habit';
 
 function App() {
+
+  // 비지니스 로직
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Habit />
     </div>
   );
 }
---
+++ react/dream-coding/src/components/Habit.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Habit extends Component {
+    render() {
+        return (
+
+            <div>
+                <span className="habit-name">Reading</span>
+                <span className="habit-count">8</span>
+            </div>
+        )
+    }
+}
