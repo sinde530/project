3dc5d3933321d73eca658d1baed4876d48e5e2aa
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-04-26 19:01:54 +0900

    habit components create

--- react/dream-coding/src/components/Habit.js
+++ react/dream-coding/src/components/Habit.js
@@ -3,10 +3,18 @@ import React, { Component } from "react";
 export default class Habit extends Component {
     render() {
         return (
-
-            <div>
+            <div className="habit-container">
                 <span className="habit-name">Reading</span>
                 <span className="habit-count">8</span>
+                <button className="habit-button habit-increase">
+                    <i className="fas fa-plus-square"></i>
+                </button>
+                <button className="habit-button habit-decrease">
+                    <i className="fas fa-plus-minus"></i>
+                </button>
+                <button className="habit-button habit-delete">
+                    <i className="fas fa-trash"></i>
+                </button>
             </div>
         )
     }
