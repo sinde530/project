a2973f7de45bbd28b834a4eec05b8e15ef2310b6
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-04-27 19:26:21 +0900

    최상위 디렉토리 변경

--- react/dream-coding/src/App.js
+++ react/dream-coding/src/App.js
@@ -1,14 +1,50 @@
+import React, { Component } from "react";
 import './App.css';
 import Index from './components/habitPlan';
+import ListItem from './components/habitPlan/ListItem';
+import Navbar from './components/Navbar';
 
-function App() {
+export default class App extends Component {
+  state = {
+    listItem: [
+      { id: 1, name: 'Reading', count: 0 },
+      { id: 2, name: 'Running', count: 0 },
+      { id: 3, name: 'Coding', count: 0 },
+    ]
+  }
 
-  // 비지니스 로직
-  return (
-    <div className="App">
-      <Index />
-    </div>
-  );
+  handleIncrement = list => {
+    // ...this = 새로운 배열을 만듬
+    const listItem = [...this.state.listItem];
+    const index = listItem.indexOf(list);
+    listItem[index].count++;
+    this.setState({ listItem });
+  }
+
+  handleDecrement = list => {
+    const listItem = [...this.state.listItem];
+    const index = listItem.indexOf(list);
+    const count = listItem[index].count - 1;
+    listItem[index].count = count < 0 ? 0 : count;
+    this.setState({ listItem });
+  }
+
+  handleDelete = list => {
+    const listItem = this.state.listItem.filter(item => item.id !== list.id);
+    this.setState({ listItem })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* <Navbar totalCount={this.props.item.filter(item => item.count > 0)} /> */}
+        <ListItem
+          habit={this.state.listItem}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+        />
+      </div>
+    )
+  }
 }
-
-export default App;
