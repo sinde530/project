ca8c9795a06e62fe68add384d7d9e428f715fb24
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-04-28 19:38:12 +0900

    css 조금 수정

--- react/dream-coding/src/index.js
+++ react/dream-coding/src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/js/all.js';
-import SimpleHabit from './components/habitPlan/SimpleHabit';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SimpleHabit />
+    <App />
   </React.StrictMode>
 );
 
