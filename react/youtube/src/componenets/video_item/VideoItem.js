ef311a7ff5ed08d5d96621e9c2463c28925d15d2
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-04-29 13:35:31 +0900

    VideoItem styled create

--- react/youtube/src/componenets/video_item/VideoItem.js
+++ react/youtube/src/componenets/video_item/VideoItem.js
@@ -1,18 +1,42 @@
+import styled from "@emotion/styled";
 import React from "react";
+
+const Video = styled.li`
+    display: flex;
+    align-items: center;
+    border: 1px solid lightgray;
+    margin-bottom: 1rem;
+    transition: transform 250ms ease-in;
+    cursor: pointer;
+    &:hover{
+        transform: scale(1.02);
+    }
+`
+const Thumbnails = styled.img`
+    width: 40%;
+`
+const Title = styled.p`
+    margin: 0;
+`
+const Channel = styled.p`
+    margin: 0;
+    font-size: 0.9rem;
+`
+
 
 function VideoItem(props) {
     return (
         <div>
-            <li>
-                <img
+            <Video>
+                <Thumbnails
                     src={props.video.snippet.thumbnails.medium.url}
                     alt="video thumbnail"
                 />
                 <div>
-                    <p>{props.video.snippet.title}</p>
-                    <p>{props.video.snippet.channelTitle}</p>
+                    <Title>{props.video.snippet.title}</Title>
+                    <Channel>{props.video.snippet.channelTitle}</Channel>
                 </div>
-            </li>
+            </Video>
         </div>
     )
 }
