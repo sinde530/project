5bed1a2c67b0036369074231d4081ebb12e3767a
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-04-29 13:38:16 +0900

    VideoList styled create

--- react/youtube/src/componenets/video_list/VideoList.js
+++ react/youtube/src/componenets/video_list/VideoList.js
@@ -1,13 +1,21 @@
+import styled from "@emotion/styled";
 import React from "react";
 import VideoItem from "../video_item/VideoItem";
 
+const Videos = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+    padding-left: 0;
+`
+
 function VideoList(props) {
     return (
-        <ul>
+        <Videos>
             {props.videos.map(video => (
                 <VideoItem key={video.id} video={video} />
             ))}
-        </ul>
+        </Videos>
     )
 }
 
