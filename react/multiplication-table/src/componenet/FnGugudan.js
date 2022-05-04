0dba7c42523d1966101f168f2ed3580b3bacbbee
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-04 20:12:19 +0900

    react hooks 사용하여 구구단 만들기

--- react/multiplication-table/src/componenet/FnGugudan.js
+++ react/multiplication-table/src/componenet/FnGugudan.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 function FnGugudan() {
+  const [first, setFirst] = useState(Math.ceil(Math.random() * 9));
+  const [second, setSecond] = useState(Math.ceil(Math.random() * 9));
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const input = useRef(null);
+
+  const handleChangeNumber = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (first * second === parseInt(value)) {
+      setResult(value + ' Correct');
+      setFirst(Math.ceil(Math.random() * 9));
+      setSecond(Math.ceil(Math.random() * 9));
+      setValue('');
+      input.current.focus();
+    } else {
+      setResult(value + ' Wrong');
+      setValue('');
+      input.current.focus();
+    }
+  };
+
+  console.log('Rendering');
   return (
     <div>
       <h1>
-        {'1'} * {'2'} =
+        {first} * {second} = ?
       </h1>
-      <form>
-        <input type="number" />
+      <form onSubmit={handleSubmit}>
+        <input type="number" onChange={handleChangeNumber} value={value} />
         <button type="submit">submit</button>
       </form>
-      <div>{'result'}</div>
+      <div>{result}</div>
     </div>
   );
 }
