22f71395ec174cf72bee55068221da9c7103f8d2
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-06 16:56:21 +0900

    q

--- react/shopping-mall/src/App.js
+++ react/shopping-mall/src/App.js
@@ -12,12 +12,33 @@ class App extends Component {
     super();
     this.state = {
       products: data,
+      cart: [],
+      quantity: 1,
+      totalAmount: 0,
     };
     this.renderFoodDetail = this.renderFoodDetail.bind(this);
+    this.handleAddToCart = this.handleAddToCart.bind(this);
+    this.checkProduct = this.checkProduct.bind(this);
   }
 
-  componentDidMount() {
-    console.log(this.state.products);
+  handleAddToCart(selectedProducts) {
+    let cartItem = this.state.cart;
+    let productID = selectedProducts.id;
+    if (this.checkProduct(productID)) {
+      let index = cartItem.findIndex((item) => {
+        return item.id === productID;
+      });
+      cartItem[index].quantity += 1;
+      this.setState({
+        cart: cartItem,
+      });
+    } else {
+      cartItem.push(selectedProducts);
+      this.setState({
+        cart: cartItem,
+        quantity: 1,
+      });
+    }
   }
 
   renderFoodDetail() {
@@ -28,6 +49,8 @@ class App extends Component {
           render={(props) => {
             return (
               <Item
+                addToCart={this.handleAddToCart}
+                productQuantity={this.state.quantity}
                 image={products.image}
                 name={products.name}
                 price={products.price}
@@ -39,6 +62,48 @@ class App extends Component {
         />
       );
     });
+  }
+
+  // 장바구니에 이미 제품이 있는지 확인하는 메소드
+  checkProduct(id) {
+    let cart = this.state.cart;
+    return cart.some((item) => {
+      return item.id === id;
+    });
+  }
+
+  sumTotalAmount() {
+    let cart = this.state.cart;
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+      if (cart[i].checked === true) {
+        total += cart[i].price * Number(cart[i].quantity);
+      }
+    }
+    this.setState({
+      totalAmount: total,
+    });
+  }
+
+  componentDidMount() {
+    let cart = localStorage.cart;
+    if (cart) {
+      this.setState(
+        (prevState) => ({
+          cart: JSON.parse(cart),
+        }),
+        function () {
+          this.sumTotalAmount();
+        }
+      );
+    }
+    console.log(this.state.products);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.cart !== this.state.cart) {
+      localStorage.cart = JSON.stringify(this.state.cart);
+    }
   }
 
   render() {
