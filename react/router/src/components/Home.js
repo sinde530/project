1827b7948bf6838aed675498558764cd362445ea
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-11 16:41:57 +0900

    기본 라우팅 설정

---
+++ react/router/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <button>go to Profiles!</button>
+    </div>
+  );
+}
+
+export default Home;
---
+++ react/router/src/components/Profiles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Profiles() {
+  return (
+    <div>
+      <h1>Profiles Page</h1>
+      <button>go to Home!</button>
+    </div>
+  );
+}
+
+export default Profiles;
