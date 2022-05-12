d122553127ae762ff5c207b2f8b102a151def3d3
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-12 14:47:32 +0900

    EOF modify

--- react/card-maker/src/service/firebase.js
+++ react/card-maker/src/service/firebase.js
@@ -1,4 +1,3 @@
-// import firebase from 'firebase';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
@@ -6,13 +5,6 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  //   storageBucket: 'business-card-maker-3916a.appspot.com',
-  //   messagingSenderId: '474309354507',
-  //   appId: '1:474309354507:web:4dd2007f2cd664fbf3130e',
-  //   measurementId: 'G-VMV8Z036HD',
 };
-
-// Initialize Firebase
-// const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export const firebaseApp = initializeApp(firebaseConfig);
