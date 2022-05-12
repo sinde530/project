95cfc3ced9cb344b74f5de2421487ba108d1cac7
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-12 12:23:51 +0900

    Login Form styles create

--- react/card-maker/src/components/login/Login.js
+++ react/card-maker/src/components/login/Login.js
@@ -1,6 +1,9 @@
 import React from 'react';
+
 import Footer from '../footer/Footer';
 import Header from '../header/Header';
+
+import styles from './login.module.css';
 
 function Login({ authService }) {
   function onLogin(e) {
@@ -9,16 +12,20 @@ function Login({ authService }) {
       .then(console.log);
   }
   return (
-    <div>
+    <div className={styles.login}>
       <Header />
       <div>
-        <h1>Login</h1>
-        <ul>
-          <li>
-            <button onClick={onLogin}>Google</button>
+        <h1>Social Login Pages</h1>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <button className={styles.google_button} onClick={onLogin}>
+              Google
+            </button>
           </li>
-          <li>
-            <button onClick={onLogin}>Github</button>
+          <li className={styles.item}>
+            <button className={styles.github_button} onClick={onLogin}>
+              Github
+            </button>
           </li>
         </ul>
       </div>
