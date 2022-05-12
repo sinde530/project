7186af8d891af7682b23d2c865cd9c53cec0b38e
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-12 11:33:12 +0900

    header design create

--- react/card-maker/src/components/header/Header.js
+++ react/card-maker/src/components/header/Header.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-function Header() {
-  return <div>Header</div>;
+import styles from './header.module.css';
+
+function Header({ onLogout }) {
+  return (
+    <header className={styles.header}>
+      {true && (
+        <button className={styles.logout} onClick={onLogout}>
+          Logout
+        </button>
+      )}
+      <img src="/images/logo.png" alt="img" className={styles.logo} />
+      <h1 className={styles.title}>Business Card Makers</h1>
+    </header>
+  );
 }
 
 export default Header;
