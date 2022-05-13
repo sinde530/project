fdd9492c9d29a44e1e840bcf24701c6b2609e6c0
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-13 15:40:21 +0900

    button components craete

--- react/card-maker/src/components/card_edit_form/CardEditForm.js
+++ react/card-maker/src/components/card_edit_form/CardEditForm.js
@@ -8,6 +8,15 @@ const CardEditForm = ({ card }) => {
   return (
     <form>
       <input type="text" name="name" value={name} />
+      <input type="text" name="company" value={company} />
+      <select name="theme" value={theme}>
+        <option value="light">Light</option>
+        <option value="dard">Dark</option>
+        <option value="colorful">colorFul</option>
+      </select>
+      <input type="text" name="title" value={title} />
+      <input type="text" name="email" value={email} />
+      <textarea name="message" value={message}></textarea>
     </form>
   );
 };
