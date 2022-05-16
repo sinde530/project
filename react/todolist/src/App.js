9ee7949fd1983676ecdb109c3707e470ff44b529
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-16 12:00:52 +0900

    아이디가 겹치지않게 handleClick했을때 지정 title 나오게 하기

--- react/todolist/src/App.js
+++ react/todolist/src/App.js
@@ -5,19 +5,28 @@ import Page from './components/Page';
 import './App.css';
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: '아무것도 하지 않기 #1',
-    },
-    {
-      id: 2,
-      title: '아무것도 하지 않기 #2',
-    },
-  ]);
+  const [state, setState] = useState({
+    newId: 100,
+    tasks: [
+      {
+        id: 1,
+        title: '아무것도 하지 않기 #1',
+      },
+      {
+        id: 2,
+        title: '아무것도 하지 않기 #2',
+      },
+    ],
+  });
+
+  const { newId, tasks } = state;
 
   function handleClick() {
     // 할일 추가
+    setState({
+      newId: newId + 1,
+      tasks: [...tasks, { id: newId, title: 'new taks' }],
+    });
   }
 
   return (
