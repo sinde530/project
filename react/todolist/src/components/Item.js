9304067007be85d767bbd51990cb5155db6dd32a
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-16 11:47:42 +0900

    title component create

---
+++ react/todolist/src/components/Item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Item({ title }) {
+  return (
+    <div>
+      <p>{title}</p>
+    </div>
+  );
+}
+
+export default Item;
--- react/todolist/src/components/List.js
+++ react/todolist/src/components/List.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Item from './Item';
 
 export default function List({ tasks }) {
   return (
     <div>
       {tasks.map((task) => (
-        <p key={task.id}>{task.title}</p>
+        <Item key={task.id} title={task.title} />
       ))}
     </div>
   );
