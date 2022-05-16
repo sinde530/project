a5fd4356d3b1fbec87fbbfcce7f709c1243039b6
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-16 11:45:36 +0900

    불필요한 코드 삭제

--- react/todolist/src/components/List.js
+++ react/todolist/src/components/List.js
@@ -1,32 +1,11 @@
 import React from 'react';
 
 export default function List({ tasks }) {
-  const Comments = [
-    {
-      id: 0,
-      name: 'Crong',
-      text: 'Work To Do #1',
-    },
-    {
-      id: 1,
-      name: 'sinde',
-      text: 'Work To Do #2',
-    },
-  ];
-
   return (
     <div>
       {tasks.map((task) => (
         <p key={task.id}>{task.title}</p>
       ))}
-      <div>
-        {Comments.map((item, index) => (
-          <li key={index}>
-            {item.name}
-            {item.text}
-          </li>
-        ))}
-      </div>
     </div>
   );
 }
