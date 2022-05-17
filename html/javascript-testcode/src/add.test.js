3e21aefd93202a1d03d7b9cd752095cbfcb13fd3
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-17 11:53:36 +0900

    npm i -D jest babel-jest @babel/core @babel/preset-env

--- html/javascript-testcode/src/add.test.js
+++ html/javascript-testcode/src/add.test.js
@@ -2,5 +2,9 @@ const add = require('./add');
 
 // test라는 함수에 원하는 내용을 작성
 test('adds, 1 + 2 to equal 3', () => {
+  // 테스트코드 작성
   expect(add(1, 2)).toBe(3);
 });
+
+// expect : expect 기능은 값을 테스트할때 주로 쓰인다
+// toBe : 정확하게 이 값이여야한다.
