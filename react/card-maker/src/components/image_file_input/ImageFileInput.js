615765e02dfd7ea4996fd68f94ad49bc169fb4dc
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-20 14:53:51 +0900

    ImageUploads console.log

--- react/card-maker/src/components/image_file_input/ImageFileInput.js
+++ react/card-maker/src/components/image_file_input/ImageFileInput.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-// import styles from './imagefileinput.module.css';
+import styles from './imagefileinput.module.css';
 
-function ImageFileInput({ ImageUpload }) {
-  return <button>Image</button>;
+function ImageFileInput({ ImageUpload, name, onFileChange }) {
+  const inputRef = useRef();
+  const onButtonClick = (e) => {
+    e.preventDefault();
+    inputRef.current.click();
+  };
+
+  const onChange = (e) => {
+    console.log(e.target.vaule[0]);
+  };
+
+  return (
+    <div className={styles.container}>
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type="file"
+        accept="image/*"
+        name="file"
+        onChange={onChange}
+      />
+      <button type="button" className={styles.button} onClick={onButtonClick}>
+        {name || 'no File'}
+      </button>
+    </div>
+  );
 }
 
 export default ImageFileInput;
