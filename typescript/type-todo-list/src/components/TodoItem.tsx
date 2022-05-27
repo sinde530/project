6bdca3abc8617a5e4f9028e9dba076fea96d7850
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-27 16:32:56 +0900

    TodoItem.tsx

--- typescript/type-todo-list/src/components/TodoItem.tsx
+++ typescript/type-todo-list/src/components/TodoItem.tsx
@@ -1,3 +1,26 @@
-export default function TodoItem() {
-  return <div>TodoItem</div>;
+import React from "react";
+
+export type TodoItemProps = {
+  todo: {
+    id: number;
+    text: string;
+  };
+};
+
+function TodoItem({ todo }: TodoItemProps) {
+  return (
+    <div>
+      <div>
+        <span>{todo.id}</span>
+      </div>
+      <div>
+        <span>{todo.text}</span>
+      </div>
+      <div>
+        <span>삭제</span>
+      </div>
+    </div>
+  );
 }
+
+export default TodoItem;
