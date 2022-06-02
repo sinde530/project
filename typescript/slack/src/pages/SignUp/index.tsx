887250c883e021bf618c10ff7ee42cf996c19c39
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-02 22:22:34 +0900

    design modify

--- typescript/slack/src/pages/SignUp/index.tsx
+++ typescript/slack/src/pages/SignUp/index.tsx
@@ -1,16 +1,35 @@
+/* eslint-disable no-console */
 import { useCallback, useState } from "react";
+
+import { Link, Redirect } from "react-router-dom";
 
 import axios from "axios";
 
-import { Form, Header, Input, Label } from "./styles";
+import {
+  Button,
+  Error,
+  Form,
+  Header,
+  Input,
+  Label,
+  LinkContainer,
+  Success,
+} from "./styles";
 
 export default function SignUp() {
-  const { data, error, revalidate } = useSWR("/api/users", fetcher);
+  //   const { data, error, revalidate } = useSWR("/api/users", fetcher);
 
-  const [email, setEmail] = useInput("");
-  const [nickname, setNickname] = useInput("");
-  const [password, setPassword] = useInput("");
-  const [passwordCheck, setPasswordCheck] = useInput("");
+  //   const [email, setEmail] = useInput("");
+  const [email, onChangeEmail] = useState("");
+
+  //   const [nickname, setNickname] = useInput("");
+  const [nickname, onChangeNickname] = useState("");
+
+  //   const [password, setPassword] = useInput("");
+  const [password, setPassword] = useState("");
+
+  //   const [passwordCheck, setPasswordCheck] = useInput("");
+  const [passwordCheck, setPasswordCheck] = useState("");
 
   const [mismatchError, setMismatchError] = useState(false);
   const [signUpSuccess, setSignUpSuccess] = useState(false);
@@ -32,40 +51,42 @@ export default function SignUp() {
     [password]
   );
 
-  const onSubmit = useCallback(
-    (e) => {
-      e.preventDefault();
-      if (!mismatchError && nickname) {
-        console.log("서버로 회원가입하기");
-        setSignUpError("");
-        setSignUpSuccess(false);
-        axios
-          .post("/api/users", {
-            email,
-            nickname,
-            password,
-          })
-          .then((response) => {
-            console.log(response);
-            setSignUpSuccess(true);
-          })
-          .catch((error) => {
-            console.log(error.response);
-            setSignUpError(error.response.data);
-          })
-          .finally(() => {});
-      }
-    },
-    [email, nickname, password, passwordCheck, mismatchError]
-  );
+  const onSubmit = useCallback((e) => {});
 
-  if (data === undefined) {
-    return <div>로딩중...</div>;
-  }
+  //   const onSubmit = useCallback(
+  //     (e) => {
+  //       e.preventDefault();
+  //       if (!mismatchError && nickname) {
+  //         console.log("서버로 회원가입하기");
+  //         setSignUpError("");
+  //         setSignUpSuccess(false);
+  //         axios
+  //           .post("/api/users", {
+  //             email,
+  //             nickname,
+  //             password,
+  //           })
+  //           .then((response) => {
+  //             console.log(response);
+  //             setSignUpSuccess(true);
+  //           })
+  //           .catch((error) => {
+  //             console.log(error.response);
+  //             setSignUpError(error.response.data);
+  //           })
+  //           .finally(() => {});
+  //       }
+  //     },
+  //     [email, nickname, password, passwordCheck, mismatchError]
+  //   );
 
-  if (data) {
-    return <Redirect to="/workspace/sleact/channel/일반" />;
-  }
+  //   if (data === undefined) {
+  //     return <div>로딩중...</div>;
+  //   }
+
+  //   if (data) {
+  //     return <Redirect to="/workspace/sleact/channel/일반" />;
+  //   }
 
   return (
     <div>
@@ -111,7 +132,7 @@ export default function SignUp() {
         </Label>
 
         <Label id="password-check-label">
-          <span>비밀번호</span>
+          <span>비밀번호 확인</span>
           <div>
             <Input
               type="password"
@@ -121,8 +142,19 @@ export default function SignUp() {
               onChange={onChangePasswordCheck}
             />
           </div>
+          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+          {!nickname && <Error>닉네임을 입력해주세요.</Error>}
+          {signUpError && <Error>{signUpError}</Error>}
+          {signUpSuccess && (
+            <Success>회원가입되었습니다! 로그인해주세요.</Success>
+          )}
         </Label>
+        <Button type="submit">회원가입</Button>
       </Form>
+      <LinkContainer>
+        이미 회원이신가요? &nbsp;
+        <Link to="/login">로그인 하러가기</Link>
+      </LinkContainer>
     </div>
   );
 }
