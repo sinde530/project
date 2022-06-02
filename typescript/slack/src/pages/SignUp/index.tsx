2b364ec91c7c7b81cfd1b75c94e8424a19891385
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-02 22:04:19 +0900

    로그인 기능 구현중

--- typescript/slack/src/pages/SignUp/index.tsx
+++ typescript/slack/src/pages/SignUp/index.tsx
@@ -1,9 +1,128 @@
-import { Header } from "./styles";
+import { useCallback, useState } from "react";
+
+import axios from "axios";
+
+import { Form, Header, Input, Label } from "./styles";
 
 export default function SignUp() {
+  const { data, error, revalidate } = useSWR("/api/users", fetcher);
+
+  const [email, setEmail] = useInput("");
+  const [nickname, setNickname] = useInput("");
+  const [password, setPassword] = useInput("");
+  const [passwordCheck, setPasswordCheck] = useInput("");
+
+  const [mismatchError, setMismatchError] = useState(false);
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
+  const [signUpError, setSignUpError] = useState("");
+
+  const onChangePassword = useCallback(
+    (e) => {
+      setPassword(e.target.value);
+      setMismatchError(e.target.value !== passwordCheck);
+    },
+    [passwordCheck]
+  );
+
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.valye);
+      setMismatchError(e.target.value !== password);
+    },
+    [password]
+  );
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!mismatchError && nickname) {
+        console.log("서버로 회원가입하기");
+        setSignUpError("");
+        setSignUpSuccess(false);
+        axios
+          .post("/api/users", {
+            email,
+            nickname,
+            password,
+          })
+          .then((response) => {
+            console.log(response);
+            setSignUpSuccess(true);
+          })
+          .catch((error) => {
+            console.log(error.response);
+            setSignUpError(error.response.data);
+          })
+          .finally(() => {});
+      }
+    },
+    [email, nickname, password, passwordCheck, mismatchError]
+  );
+
+  if (data === undefined) {
+    return <div>로딩중...</div>;
+  }
+
+  if (data) {
+    return <Redirect to="/workspace/sleact/channel/일반" />;
+  }
+
   return (
     <div>
       <Header>Sign Up</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={onChangeEmail}
+            />
+          </div>
+        </Label>
+
+        <Label id="nickname-label">
+          <span>닉네임</span>
+          <div>
+            <Input
+              type="text"
+              id="nickname"
+              name="nickname"
+              value={nickname}
+              onChange={onChangeNickname}
+            />
+          </div>
+        </Label>
+
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={onChangePassword}
+            />
+          </div>
+        </Label>
+
+        <Label id="password-check-label">
+          <span>비밀번호</span>
+          <div>
+            <Input
+              type="password"
+              id="password-check"
+              name="password-check"
+              value={passwordCheck}
+              onChange={onChangePasswordCheck}
+            />
+          </div>
+        </Label>
+      </Form>
     </div>
   );
 }
