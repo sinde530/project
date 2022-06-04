c076097fe89afb4d2385f5c26d8901a62276bc25
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-05 01:09:30 +0900

    initialStete @typescript-eslint-default-param-last: 0 상태값을 초기화하고 카운터관리값에 카운터가 0이 들어가는데 타입스크립트에서는 다르게 적용이 되는거같다.

--- typescript/redux-typescript-example/src/modules/counter.ts
+++ typescript/redux-typescript-example/src/modules/counter.ts
@@ -12,44 +12,54 @@ const INCREASE_BY = "counter/INCREASE_BY" as const;
 
 // action function Add.
 export const increase = () => ({
-    type: INCREASE
+  type: INCREASE,
 });
 
 export const decrease = () => ({
-    type: DECREASE
+  type: DECREASE,
 });
 
 // InitialState Actions Actually most Values, modern `payload` nothink smae names.
 export const increaseBy = (diff: number) => ({
-    type: INCREASE_BY,
-    // FLUX 규칙 설명
-    // https://github.com/redux-utilities/flux-standard-action
-    payload:diff
+  type: INCREASE_BY,
+  // FLUX 규칙 설명
+  // https://github.com/redux-utilities/flux-standard-action
+  payload: diff,
 });
 
 // action renderers type request.
-type CounterAction = 
-| ReturnType<typeof increase>
-| ReturnType<typeof decrease>
-| ReturnType<typeof increaseBy>
+type CounterAction =
+  | ReturnType<typeof increase>
+  | ReturnType<typeof decrease>
+  | ReturnType<typeof increaseBy>;
 
 // redux Modules props state type actions.
 type CounterState = {
-    count : number
-}
+  count: number;
+};
 
 // InitialState Funtion
-
-const InitialState: CounterState = {
-    count: 0
-}
+const initialState: CounterState = {
+  count: 0,
+};
 
 // Get by Write toBe Reducer.
 // Reducer more state and functions setState Values Same Writing.
 // Actions We other maked CounterAction Type Settings.
 function counter(
-    state: CounterState = InitialState,
-    action: CounterAction
-)
+  state: CounterState = initialState,
+  action: CounterAction
+): CounterState {
+  switch (action.type) {
+    case INCREASE: // case 라고 입력하고 Ctrl + Space 를 누르면 어떤 종류의 action.type들이 있는지 확인 할 수 있습니다.
+      return { count: state.count + 1 };
+    case DECREASE:
+      return { count: state.count - 1 };
+    case INCREASE_BY:
+      return { count: state.count + action.payload };
+    default:
+      return state;
+  }
+}
 
-export defualt {}
+export default counter;
