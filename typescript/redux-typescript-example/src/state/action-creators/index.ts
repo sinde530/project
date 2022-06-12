9e1d76ec671bf86d23977dd28cb45744b4b95edf
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-12 18:43:29 +0900

    './bankReducer' 모듈에 내보낸 멤버 'bankReducer'이(가) 없습니다. 대신 './bankReducer에서 bankReducer 가져오기'를 사용하시겠습니까?ts(2614)

---
+++ typescript/redux-typescript-example/src/state/action-creators/index.ts
@@ -0,0 +1,33 @@
+import { Dispatch } from "react";
+
+import { ActionType } from "../action-types";
+
+import { Action } from "../actions";
+
+export const depositMoney = (amount: number) => {
+  return (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.DEPOSIT,
+      payload: amount,
+    });
+  };
+};
+
+export const withdrawMoney = (amount: number) => {
+  return (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.WITHDRAW,
+      payload: amount,
+    });
+  };
+};
+
+export const bankrupt = (amount: number) => {
+  return (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BANKRUPT,
+    });
+  };
+};
+
+export default {};
--- typescript/redux-typescript-example/src/state/reducres/index.ts
+++ typescript/redux-typescript-example/src/state/reducres/index.ts
@@ -1,4 +1,13 @@
 import { combineReducers } from "redux";
 
+import { bankReducer } from "./bankReducer";
+
 // ## 다중 감속기를 사용하려면 main.ts 파일을 만들어 `combineReducers` - redux 사용.
 // - dependencies
+
+const reducers = combineReducers({
+  // object create
+  bank: bankReducer,
+});
+
+export default reducers;
