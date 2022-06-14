ac4a5c721fd1ae20c255876582830a22ff967e77
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-14 19:17:11 +0900

    jest-environment-jsdom

--- react/tdd-settings/jest.config.js
+++ react/tdd-settings/jest.config.js
@@ -3,9 +3,28 @@
  * https://jestjs.io/docs/configuration
  */
 
+// module.exports = {
+//   clearMocks: true,
+//   collectCoverage: true,
+//   coverageDirectory: 'coverage',
+//   testEnvironment: 'jsdom',
+// };
+
 module.exports = {
-  clearMocks: true,
   collectCoverage: true,
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'json'],
+  transform: {
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+  },
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testMatch: [
+    '<rootDir>/**/*.test.(js|jsx|ts|tsx)',
+    '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))',
+  ],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
