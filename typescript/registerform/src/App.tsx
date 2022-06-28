10f67fa6cea8c4d8d7590bd340c632a92c1e4aac
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-29 01:41:31 +0900

    recoil, router-dom-v6 study

--- typescript/registerform/src/App.tsx
+++ typescript/registerform/src/App.tsx
@@ -1,16 +1,28 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+import { RecoilRoot } from "recoil";
 
 import Login from "./components/Login";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
+function Layout() {
+  return (
+    <RecoilRoot>
+      <Outlet />
+    </RecoilRoot>
+  );
+}
+
 export default function App() {
   return (
     <>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
       <Login />
     </>
--- typescript/registerform/src/main.tsx
+++ typescript/registerform/src/main.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+
 import ReactDOM from "react-dom/client";
+
 import { BrowserRouter } from "react-router-dom";
+
+import { RecoilRoot } from "recoil";
+
 import App from "./App";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>
 );
