521cd14f1f1079be0f400b3576a4157e694fa8f4
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-07-29 22:02:43 +0900

    eslint initial Commit

--- typescript/pokemon/src/App.tsx
+++ typescript/pokemon/src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,7 +16,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
