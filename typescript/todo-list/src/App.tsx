239e4dc88aacf41cb16e9c520075d3c0cd56b6c4
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-08-15 15:25:51 +0900

    eslint settings

--- typescript/todo-list/src/App.tsx
+++ typescript/todo-list/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return  (                     
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -13,8 +13,8 @@ function App() {
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+        </a>        
+      </div>    
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- typescript/todo-list/vite.config.ts
+++ typescript/todo-list/vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import envCompatible from "vite-plugin-env-compatible";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    envCompatible({
-      prefix: "VITE_",
-      mountedPath: "process.env",
-      ignoreProcessEnv: false,
-    }),
-  ],
+  plugins: [react(), tsconfigPaths()],
 });
