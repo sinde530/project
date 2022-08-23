058b0383f237345c7817e5ab028ef1b0c4a49159
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-08-23 15:29:39 +0900

    src path settings

--- typescript/card-maker/src/App.tsx
+++ typescript/card-maker/src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 
 import './App.css';
+import Test from 'src/components/Test';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -29,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Test />
     </div>
   );
 }
