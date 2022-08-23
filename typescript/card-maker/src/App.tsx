233775042ed5313ec5bba47b06fbc13f69c172f8
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-08-23 16:46:56 +0900

    Create Maker Component

--- typescript/card-maker/src/App.tsx
+++ typescript/card-maker/src/App.tsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Maker from './components/Maker';
 
-function App() {
+interface Props {
+  authService: any;
+}
+
+function App({ authService }: Props) {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login authService={authService} />} />
+        <Route path="/maker" element={<Maker />} />
       </Routes>
     </div>
   );
