15489394949bb6f0e6e009b7b98d29a04d1e5e94
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-08-24 14:27:17 +0900

    Add Main.tsx ImageUploader

--- typescript/card-maker/src/main.tsx
+++ typescript/card-maker/src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 
+import ImageUpload from './api/image_uploader';
 import AuthService from './api/auth_service';
 import { firebaseApp } from './api/firebase';
 
 import App from './App';
+import ImageFileInput from './components/Editor/ImageFileInput';
 
 import './index.css';
 
@@ -13,11 +15,15 @@ import './index.css';
  * firebaseApp 0개 인수 error
  */
 const authService = new AuthService(firebaseApp);
+const ImageUploader = new ImageUpload();
+const FileInput = (props: any) => (
+  <ImageFileInput {...props} ImageUpload={ImageUploader} />
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <React.StrictMode>
-      <App authService={authService} />
+      <App authService={authService} FileInput={FileInput} />
     </React.StrictMode>
   </BrowserRouter>,
 );
