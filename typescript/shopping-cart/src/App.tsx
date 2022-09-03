3f7ed2c184f182733437732e789459f0ec6ea07f
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-09-03 10:50:20 +0900

    prototype header

--- typescript/shopping-cart/src/App.tsx
+++ typescript/shopping-cart/src/App.tsx
@@ -2,12 +2,22 @@ import { useState } from "react";
 import { useQuery } from "react-query";
 
 // Components
-import Drawer from '@'
+// import Drawer from "@";
 
 export default function App() {
   return (
     <div>
-      <h2>App gage.</h2>
+      <header>
+        <h2>Logo</h2>
+        <p>Logo description</p>
+        <ul>
+          <li>login</li>
+          <li>list Item1</li>
+          <li>list Item2</li>
+          <li>list Item3</li>
+          <li>list Item4</li>
+        </ul>
+      </header>
     </div>
   );
 }
