d5ee550fa785b7db3637846b2cb02df5abea9611
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-09-12 18:21:46 +0900

    Create App.test.tsx

--- typescript/shopping-cart/src/App.test.tsx
+++ typescript/shopping-cart/src/App.test.tsx
@@ -2,8 +2,11 @@ import { render } from "@testing-library/react";
 
 import App from "./App";
 
-describe("", () => {
-  it("", () => {
-    render(<App />);
+describe("<App/>", () => {
+  it("renders App Component", () => {
+    const user = "Jemi";
+    const { getByText } = render(<App />);
+
+    expect(getByText(user)).toBeInTheDocument();
   });
 });
