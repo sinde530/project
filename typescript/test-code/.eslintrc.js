6ca79814ec1a803d5c718156344c026f9aa150f5
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-09-24 00:40:45 +0900

    eslint est

--- typescript/test-code/.eslintrc.js
+++ typescript/test-code/.eslintrc.js
@@ -1,28 +1,25 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  parserOption: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 13,
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
     'airbnb',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
     'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
   ],
-  env: {
-    browser: true,
-    es2021: true,
-  },
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'on',
-    '@typescript-eslint/explicit-function-return-type': 'on',
-    '@typescript-eslint/explicit-module-boundary-types': 'on',
-    '@typescript-eslint/no-explicit-any': 'on',
+    'linebreak-style': 0,
+    'import/prefer-default-export': 0,
+    'prettier/prettier': 0,
+    'import/extensions': 0,
+    'no-use-before-define': 0,
+    'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
+    'no-shadow': 0,
+    'react/prop-types': 0,
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
   },
 };
---
+++ typescript/test-code/README.md
@@ -0,0 +1,3 @@
+```
+yarn add eslint prettier @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-config-airbnb eslint-config-prettier eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-jsx-a11y eslint-plugin-import
+```
