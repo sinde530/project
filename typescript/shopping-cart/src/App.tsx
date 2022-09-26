61894801fab0fa0461316b338c89e3ad427ed800
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-09-26 12:51:46 +0900

    Create cats

--- typescript/shopping-cart/src/App.tsx
+++ typescript/shopping-cart/src/App.tsx
@@ -1,10 +1,16 @@
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import { postCats } from "./api/cats";
 
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 
 export default function App() {
+  useEffect(() => {
+    postCats();
+  }, []);
+
   return (
     <Routes>
       <Route index element={<Home />} />
