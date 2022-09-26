d880f05d50ff60d7e21cfc6c926b918a86df7387
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-09-26 10:10:32 +0900

    wirte test for

--- typescript/test-code/src/components/Sum/index.test.ts
+++ typescript/test-code/src/components/Sum/index.test.ts
@@ -11,4 +11,30 @@ describe('<Sum/>', () => {
     // undefine가 포함되어 있으면 실패로 처리한다.
     expect(Sum(3, 6)).toStrictEqual(9);
   });
+
+  it('object assigment', () => {
+    // object test
+    // 객체의 값을 확인할때 사용
+    const data = { one: 1 };
+    data.two = 2;
+    expect(data).toEqual({ one: 1, two: 2 });
+  });
+});
+
+describe('adding positive numbers', () => {
+  it('Value match', () => {
+    for (let a = 1; a < 10; a++) {
+      for (let b = 1; b < 10; b++) {
+        // 테스트 방식이 1이 10번 돌아가는거라 a나 b는 결과값이 1로만 나온다.
+        expect(a + b).toBe(2);
+      }
+    }
+  });
+  it('Value mismatch', () => {
+    for (let a = 1; a < 10; a++) {
+      for (let b = 1; b < 10; b++) {
+        expect(a + b).not.toBe(0);
+      }
+    }
+  });
 });
