92eabedd8bf6be3c6bda9c9bba103b6d7f7792f6
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-09-26 09:18:53 +0900

    toStrictEqual

--- typescript/test-code/src/components/Sum/index.test.ts
+++ typescript/test-code/src/components/Sum/index.test.ts
@@ -6,6 +6,9 @@ describe('<Sum/>', () => {
     expect(Sum(1, 2)).toBe(3);
 
     // 같은 값인지 비교
-    expect(Sum(2, 4)).toEqual(3);
+    expect(Sum(2, 4)).toEqual(6);
+
+    // undefine가 포함되어 있으면 실패로 처리한다.
+    expect(Sum(3, 6)).toStrictEqual(9);
   });
 });
