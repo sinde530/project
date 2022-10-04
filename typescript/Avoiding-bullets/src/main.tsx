2465245660d23293904e31af7affef81278693f1
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-10-04 13:54:18 +0900

    [Modify] Develop Settings Off
    렌더링이 2번되는 현상때문에 잠시 주석처리 해놓음.

--- typescript/Avoiding-bullets/src/main.tsx
+++ typescript/Avoiding-bullets/src/main.tsx
@@ -4,7 +4,8 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  // TODO : 렌더링이 2번되어 개발할때 보기편하게 주석처리 함.
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 )
