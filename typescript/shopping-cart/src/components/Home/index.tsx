ae2e3755000bd801877961b51a2f0ecfa504b8d6
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-10-04 17:40:45 +0900

    [Modify] MainPage

--- typescript/shopping-cart/src/components/Home/index.tsx
+++ typescript/shopping-cart/src/components/Home/index.tsx
@@ -1,36 +1,8 @@
-import { Link } from "react-router-dom";
-
-import { Container, Header, Logo, Ul, Li } from "./styles";
+import { Container } from "./styles";
 
 export default function Home() {
-  const user = false;
-
   return (
     <Container>
-      <Header>
-        <Logo>Logo</Logo>
-        <Ul>
-          <Li>list Item1</Li>
-          <Li>list Item2</Li>
-          <Li>list Item3</Li>
-          <Li>list Item4</Li>
-          <Li>list Item5</Li>
-          <Li>list Item6</Li>
-          <Li>list Item7</Li>
-          {user ? (
-            <Li>{user}</Li>
-          ) : (
-            <>
-              <Li>
-                <Link to="/login">로그인</Link>
-              </Li>
-              <Li>
-                <Link to="/register">회원가입</Link>
-              </Li>
-            </>
-          )}
-        </Ul>
-      </Header>
       <section>???</section>
     </Container>
   );
