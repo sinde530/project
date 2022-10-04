e36e4ce8c83f978a07f452c1a7ec5d3d3e7ed428
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-10-04 17:42:16 +0900

    [Modify] `Login` Component

--- typescript/shopping-cart/src/components/Login/index.tsx
+++ typescript/shopping-cart/src/components/Login/index.tsx
@@ -1,7 +1,35 @@
+import {
+  Box,
+  Button,
+  ButtonWrapper,
+  Container,
+  Input,
+  InputBox,
+  LoginText,
+  P,
+} from "./styled";
+
 export default function Login() {
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <Container>
+      <LoginText>로그인</LoginText>
+      <InputBox>
+        <Input type="email" placeholder="아이디" />
+      </InputBox>
+      <InputBox>
+        <Input type="password" placeholder="비밀번호" />
+      </InputBox>
+      <Box>
+        <P>
+          <a href="/">비밀번호 찾기</a>
+        </P>
+        <P>
+          <a href="/">회원가입</a>
+        </P>
+      </Box>
+      <ButtonWrapper>
+        <Button type="submit">로그인</Button>
+      </ButtonWrapper>
+    </Container>
   );
 }
