4df5c419f9f69c8c17c561616c3279d08f1c7345
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-10-08 16:12:39 +0900

    [Remove] `useEffect()`

--- typescript/shopping-cart/src/App.tsx
+++ typescript/shopping-cart/src/App.tsx
@@ -1,20 +1,14 @@
-import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
 
-import { postCats } from "./api/cats";
-
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
+
 import MainLayout from "./layouts/MainLayout";
 
 export default function App() {
-  useEffect(() => {
-    postCats();
-  }, []);
-
   return (
     <Routes>
       <Route element={<MainLayout />}>
