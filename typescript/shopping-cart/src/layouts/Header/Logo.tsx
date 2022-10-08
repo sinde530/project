52ecd983f9e156c2bf73cf9035636f305af07213
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-10-08 17:15:04 +0900

    [Modify] Header `Logo Text Style`

--- typescript/shopping-cart/src/layouts/Header/Logo.tsx
+++ typescript/shopping-cart/src/layouts/Header/Logo.tsx
@@ -1,15 +1,21 @@
 import styled from "@emotion/styled";
+
 import { Link } from "react-router-dom";
 
 const Container = styled.div({
-  padding: "16px 16px",
+  margin: "16px 16px",
+});
+
+const LogoText = styled.p({
+  fontSize: "24px",
+  fontWeight: "500",
 });
 
 export default function Logo() {
   return (
     <Container>
       <Link to="/">
-        <p>Logo</p>
+        <LogoText>Testing Library</LogoText>
       </Link>
     </Container>
   );
