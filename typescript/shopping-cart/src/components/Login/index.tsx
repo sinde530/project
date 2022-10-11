ea0db8fc1fdeefec29dcbfa8836a620ae6f687a8
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-10-11 22:48:40 +0900

    [Create] Google Login Button

--- typescript/shopping-cart/src/components/Login/index.tsx
+++ typescript/shopping-cart/src/components/Login/index.tsx
@@ -1,32 +1,37 @@
+import GoogleLogin from "react-google-login";
 import {
-  Box,
   Button,
   ButtonWrapper,
   Container,
   Input,
   InputBox,
   LoginText,
-  P,
 } from "./styled";
 
 export default function Login() {
+  const failedGoogle = (res: any) => {
+    console.error(res);
+  };
+
+  const successLogin = () => {
+    alert("성공");
+  };
+
   return (
     <Container>
       <LoginText>로그인</LoginText>
       <InputBox>
+        <GoogleLogin
+          clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
+          buttonText="구글로 계속하기"
+          onSuccess={successLogin}
+          onFailure={failedGoogle}
+        />
         <Input type="email" placeholder="아이디" />
       </InputBox>
       <InputBox>
         <Input type="password" placeholder="비밀번호" />
       </InputBox>
-      <Box>
-        <P>
-          <a href="/">비밀번호 찾기</a>
-        </P>
-        <P>
-          <a href="/register">회원가입</a>
-        </P>
-      </Box>
       <ButtonWrapper>
         <Button type="submit">로그인</Button>
       </ButtonWrapper>
