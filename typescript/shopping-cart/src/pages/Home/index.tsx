5d49ff113d68064c7119822a573bb1045bd70ba1
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-10-11 23:13:39 +0900

    [Modify] Home Layouts SubComponents Styles

--- typescript/shopping-cart/src/pages/Home/index.tsx
+++ typescript/shopping-cart/src/pages/Home/index.tsx
@@ -1,15 +1,5 @@
-import { Link } from "react-router-dom";
-
-import {
-  Container,
-  Section,
-  Title,
-  CardBox,
-  Image,
-  TextBox,
-  Description,
-  Price,
-} from "./styles";
+import Product from "src/components/Product";
+import { Container, Section, Title } from "./styles";
 
 import Carousel from "../../components/Carousel";
 
@@ -19,79 +9,7 @@ export default function Home() {
       <Carousel />
       <Section>
         <Title>WEEKLY BEST</Title>
-        <CardBox>
-          <Link to="/">
-            <Image
-              src="http://cdn2-aka.makeshop.co.kr/shopimages/jogunshop/040002001559.gif?1628221767"
-              alt="images"
-            />
-            <TextBox>
-              <Description>청바지 ㄱㄴㄷㄹㅁㅂㅅ ㄱㄴㄷㄹㅁㅂㅅ</Description>
-              <Price>14,300</Price>
-            </TextBox>
-          </Link>
-        </CardBox>
-
-        <CardBox>
-          <Link to="/">
-            <Image
-              src="http://cdn2-aka.makeshop.co.kr/shopimages/jogunshop/040002001559.gif?1628221767"
-              alt="images"
-            />
-            <TextBox>
-              <Description>청바지 ㄱㄴㄷㄹㅁㅂㅅ ㄱㄴㄷㄹㅁㅂㅅ</Description>
-              <Price>14,300</Price>
-            </TextBox>
-          </Link>
-        </CardBox>
-        <CardBox>
-          <Link to="/">
-            <Image
-              src="http://cdn2-aka.makeshop.co.kr/shopimages/jogunshop/040002001559.gif?1628221767"
-              alt="images"
-            />
-            <TextBox>
-              <Description>청바지 ㄱㄴㄷㄹㅁㅂㅅ ㄱㄴㄷㄹㅁㅂㅅ</Description>
-              <Price>14,300</Price>
-            </TextBox>
-          </Link>
-        </CardBox>
-        <CardBox>
-          <Link to="/">
-            <Image
-              src="http://cdn2-aka.makeshop.co.kr/shopimages/jogunshop/040002001559.gif?1628221767"
-              alt="images"
-            />
-            <TextBox>
-              <Description>청바지 ㄱㄴㄷㄹㅁㅂㅅ ㄱㄴㄷㄹㅁㅂㅅ</Description>
-              <Price>14,300</Price>
-            </TextBox>
-          </Link>
-        </CardBox>
-        <CardBox>
-          <Link to="/">
-            <Image
-              src="http://cdn2-aka.makeshop.co.kr/shopimages/jogunshop/040002001559.gif?1628221767"
-              alt="images"
-            />
-            <TextBox>
-              <Description>청바지 ㄱㄴㄷㄹㅁㅂㅅ ㄱㄴㄷㄹㅁㅂㅅ</Description>
-              <Price>14,300</Price>
-            </TextBox>
-          </Link>
-        </CardBox>
-        <CardBox>
-          <Link to="/">
-            <Image
-              src="http://cdn2-aka.makeshop.co.kr/shopimages/jogunshop/040002001559.gif?1628221767"
-              alt="images"
-            />
-            <TextBox>
-              <Description>청바지 ㄱㄴㄷㄹㅁㅂㅅ ㄱㄴㄷㄹㅁㅂㅅ</Description>
-              <Price>14,300</Price>
-            </TextBox>
-          </Link>
-        </CardBox>
+        <Product />
       </Section>
     </Container>
   );
--- typescript/shopping-cart/src/pages/Home/styles.ts
+++ typescript/shopping-cart/src/pages/Home/styles.ts
@@ -20,35 +20,3 @@ export const Title = styled.h1({
   textAlign: "center",
   paddingBottom: "1rem",
 });
-
-export const CardBox = styled.div({
-  display: "inline-block",
-  width: "344px",
-  padding: "8px",
-
-  "@media(max-width:720px)": {
-    width: "159px",
-  },
-});
-
-export const Image = styled.img({
-  width: "100%",
-  borderRadius: "10px",
-  overflow: "hidden",
-});
-
-export const TextBox = styled.div({
-  marginTop: "7px",
-});
-
-export const Description = styled.p({
-  fontWeight: "300",
-  fontSize: "16px",
-  lineHeight: "23px",
-});
-
-export const Price = styled.p({
-  fontWeight: "500",
-  lineHeight: "19px",
-  fontSize: "18px",
-});
