287fd051496775860fb22c2d7c67004d4e313493
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-10-13 15:36:40 +0900

    [Create] ThemeIcon

--- typescript/shopping-cart/src/App.tsx
+++ typescript/shopping-cart/src/App.tsx
@@ -1,6 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
 import "./App.css";
+
+import { ThemeProvider } from "styled-components";
+
+import GlobalStyles from "./GlobalStyles";
+import { darkTheme, lightTheme } from "./theme";
+import ThemeIcon from "./components/ThemeIcon";
 
 import Home from "./pages/Home";
 import Login from "./components/Login";
@@ -10,14 +17,24 @@ import NotFound from "./pages/404/index";
 import MainLayout from "./layouts/MainLayout";
 
 export default function App() {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+
+  const toggleDarkMode = (): void => {
+    setIsDarkMode((click) => !click);
+  };
+
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <ThemeIcon isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
   );
 }
