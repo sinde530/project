99a97ab7c409c56e051d2cc811dadd4999b942c6
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-10-13 14:01:23 +0900

    [Create] App.tsx

--- typescript/theme/src/App.tsx
+++ typescript/theme/src/App.tsx
@@ -1,28 +1,45 @@
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+
 import reactLogo from './assets/react.svg';
+
+import GlobalStyles from './GlobalStyles';
+import { darkTheme, lightTheme } from './theme';
+import ThemeIcon from './components/ThemeIcon';
 
 import './App.css';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+
+  const toggleDarkMode = (): void => {
+    setIsDarkMode((click) => !click);
+  };
+
   return (
-    <div className="App">
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <ThemeIcon isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <div>
+          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </ThemeProvider>
   );
 }
 
