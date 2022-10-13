c30f622d9cbbe8835afc7080804e8369e8a22964
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-10-13 14:00:03 +0900

    [Create] `theme.ts`

--- typescript/theme/src/theme.ts
+++ typescript/theme/src/theme.ts
@@ -1,18 +1,15 @@
-export const lightTheme = {
-  bgColor: '#fff',
-  textColor: '#222',
-  accentColor: '#12cbef',
+import { DefaultTheme } from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+  color: '#ffffff',
+  bgColor: '#202020',
+  boxShadow: 'none',
+  border: '1px solid #505050',
 };
 
-export const darkTheme = {
-  bgColor: '#282c35',
-  textColor: '#fff',
-  accentColor: '#ffe246',
+export const lightTheme: DefaultTheme = {
+  color: '#000000',
+  bgColor: '#ffffff',
+  boxShadow: '0 0 10px -3px #a0a0a0',
+  border: 'none',
 };
-
-export const theme = {
-  lightTheme,
-  darkTheme,
-};
-
-export default theme;
