6be85c195608a669cf60689e9f39335ee5fdd684
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-01-09 16:06:49 +0900

    index.html title changed

--- typescript/snake-game/src/App.tsx
+++ typescript/snake-game/src/App.tsx
@@ -3,9 +3,5 @@ import "./App.css";
 import SnakeGame from "src/components/SnakeGame";
 
 export default function App() {
-  return (
-    <div>
-      <SnakeGame />
-    </div>
-  );
+  return <SnakeGame />;
 }
--- typescript/snake-game/src/components/SnakeGame/Controller.tsx
+++
@@ -1,3 +0,0 @@
-export default function Controller() {
-  return <div>Controller</div>;
-}
