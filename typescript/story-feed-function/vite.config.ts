e09eed4c73f6b2cb0cd974f421b123f816b1fb06
Sinde530 <70146617+sinde530@users.noreply.github.com>
2023-07-18 23:43:49 +0900

    Initial Commits

--- typescript/story-feed-function/vite.config.ts
+++ typescript/story-feed-function/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3090'
+    },
+			host: '0.0.0.0',
+			port: 3000,
+  }
 })
