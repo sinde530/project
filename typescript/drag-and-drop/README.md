271419665e72250e21b40fd2a3910296f4b31e0f
Sinde530 <70146617+sinde530@users.noreply.github.com>
2023-10-03 23:57:53 +0900

    Drag And Drop Failed

--- typescript/drag-and-drop/README.md
+++ typescript/drag-and-drop/README.md
@@ -3,3 +3,5 @@
 yarn add react-beautiful-dnd 
 yarn add -D @types/react-beautiful-dnd
 ```
+
+# react-beautiful-dnd 실행 안됌.
--- typescript/drag-and-drop/src/App.tsx
+++ typescript/drag-and-drop/src/App.tsx
@@ -1,34 +1,91 @@
 import { useState } from 'react';
-import viteLogo from '../../../../../../../vite.svg';
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from 'react-beautiful-dnd';
 import './App.css';
-import reactLogo from './assets/react.svg';
+
+const listItems = [
+  {
+    id: '1',
+    name: 'Study Spanish',
+  },
+  {
+    id: '2',
+    name: 'Workout',
+  },
+  {
+    id: '3',
+    name: 'Film Youtube',
+  },
+  {
+    id: '4',
+    name: 'Grocery Shop',
+  },
+];
+
+const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
+  padding: 10,
+  margin: `0 50px 15px 50px`,
+  background: isDragging ? '#4a2975' : 'white',
+  color: isDragging ? 'white' : 'black',
+  border: `1px solid black`,
+  fontSize: `20px`,
+  borderRadius: `5px`,
+
+  ...draggableStyle,
+});
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todo, setTodo] = useState(listItems);
 
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result;
+    if (!destination) return;
+
+    const items = Array.from(todo);
+    const [newOrder] = items.splice(source.index, 1);
+    items.splice(destination.index, 0, newOrder);
+
+    setTodo(items);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <h1>Drag and Drop</h1>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="todo">
+          {(provided) => (
+            <div
+              className="todo"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {todo.map(({ id, name }, index) => {
+                return (
+                  <Draggable key={id} draggableId={id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style,
+                        )}
+                      >
+                        {name}
+                      </div>
+                    )}
+                  </Draggable>
+                );
+              })}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
   );
 }
 
