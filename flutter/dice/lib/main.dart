fa9ded6b265aad9917cd96850e1b604c0d7fd7b0
Sinde530 <70146617+sinde530@users.noreply.github.com>
2023-12-06 16:03:29 +0900

    Add Text Styles

--- flutter/dice/lib/main.dart
+++ flutter/dice/lib/main.dart
@@ -2,11 +2,24 @@ import "package:flutter/material.dart";
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
+        // backgroundColor: Colors.amber,
+        body: Container(
+          decoration: const BoxDecoration(
+            // 그라디언트
+            gradient: LinearGradient(
+                colors: [Colors.red, Colors.blue],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          ),
           // 디스플레이 중간에 추가해줌.
-          child: Text("Hello world"),
+          child: const Center(
+            child: Text(
+              "Hello World",
+              style: TextStyle(color: Colors.amber, fontSize: 28),
+            ),
+          ),
         ),
       ),
     ),
