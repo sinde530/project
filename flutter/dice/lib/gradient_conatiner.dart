15188aa4f8fe4ef21e6e8269ea9314c0f1d427b6
Sinde530 <70146617+sinde530@users.noreply.github.com>
2023-12-06 17:33:27 +0900

    Modify LinearGradient Colors props

--- flutter/dice/lib/gradient_conatiner.dart
+++ flutter/dice/lib/gradient_conatiner.dart
@@ -1,29 +1,30 @@
 import 'package:dice/styled_text.dart';
 import 'package:flutter/material.dart';
 
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(context) {
-    // var를 이용하여, 데이터를 재 할당 할수있게 해주자.
-    startAlignment = Alignment.center;
-    endAlignment = Alignment.center;
-
     return Container(
       decoration: BoxDecoration(
         // 그라디언트
         gradient: LinearGradient(
-            colors: const [Colors.red, Colors.blue],
-            begin: startAlignment,
-            end: endAlignment),
+          // colors: [Colors.red, Colors.blue],
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
       ),
       // 디스플레이 중간에 추가해줌.
       child: const Center(
-        child: StyledText(),
+        child: StyledText("123"),
       ),
     );
   }
