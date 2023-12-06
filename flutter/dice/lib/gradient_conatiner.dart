84a9b3dd4db41934628fa977938e8c92070b5a9d
Sinde530 <70146617+sinde530@users.noreply.github.com>
2023-12-06 17:02:05 +0900

    Add gradient_container.dart file

--- flutter/dice/lib/gradient_conatiner.dart
+++ flutter/dice/lib/gradient_conatiner.dart
@@ -1,3 +1,4 @@
+import 'package:dice/styled_text.dart';
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget {
@@ -15,10 +16,7 @@ class GradientContainer extends StatelessWidget {
       ),
       // 디스플레이 중간에 추가해줌.
       child: const Center(
-        child: Text(
-          "Hello World",
-          style: TextStyle(color: Colors.amber, fontSize: 28),
-        ),
+        child: StyledText(),
       ),
     );
   }
