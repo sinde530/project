56432c852302d6c277fb73475005111570622bc1
Sinde530 <70146617+sinde530@users.noreply.github.com>
2023-12-06 17:07:52 +0900

    Modify start,end Alignment datas

--- flutter/dice/lib/gradient_conatiner.dart
+++ flutter/dice/lib/gradient_conatiner.dart
@@ -1,18 +1,25 @@
 import 'package:dice/styled_text.dart';
 import 'package:flutter/material.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
+    // var를 이용하여, 데이터를 재 할당 할수있게 해주자.
+    startAlignment = Alignment.center;
+    endAlignment = Alignment.center;
+
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         // 그라디언트
         gradient: LinearGradient(
-            colors: [Colors.red, Colors.blue],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight),
+            colors: const [Colors.red, Colors.blue],
+            begin: startAlignment,
+            end: endAlignment),
       ),
       // 디스플레이 중간에 추가해줌.
       child: const Center(
