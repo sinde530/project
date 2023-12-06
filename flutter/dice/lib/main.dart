a5682f8609b0985e585564d02f3af6e26a832414
Sinde530 <70146617+sinde530@users.noreply.github.com>
2023-12-06 15:23:15 +0900

    Add runApp

--- flutter/dice/lib/main.dart
+++ flutter/dice/lib/main.dart
@@ -1,1 +1,14 @@
+import "package:flutter/material.dart";
 
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          // 디스플레이 중간에 추가해줌.
+          child: Text("Hello world"),
+        ),
+      ),
+    ),
+  );
+}
