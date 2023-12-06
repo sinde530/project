7ea9cb85bf825b6e363f20d4554ea640acdd9479
Sinde530 <70146617+sinde530@users.noreply.github.com>
2023-12-06 16:40:00 +0900

    Add gradient_container dart file

--- flutter/dice/lib/main.dart
+++ flutter/dice/lib/main.dart
@@ -1,26 +1,11 @@
+import "package:dice/gradient_conatiner.dart";
 import "package:flutter/material.dart";
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        // backgroundColor: Colors.amber,
-        body: Container(
-          decoration: const BoxDecoration(
-            // 그라디언트
-            gradient: LinearGradient(
-                colors: [Colors.red, Colors.blue],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight),
-          ),
-          // 디스플레이 중간에 추가해줌.
-          child: const Center(
-            child: Text(
-              "Hello World",
-              style: TextStyle(color: Colors.amber, fontSize: 28),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
