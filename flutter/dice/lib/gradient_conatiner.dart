28d290266b6013f366826422e3fba28ba65c53cb
Sinde530 <70146617+sinde530@users.noreply.github.com>
2023-12-06 17:54:14 +0900

    Add rollDice image upload

--- flutter/dice/lib/gradient_conatiner.dart
+++ flutter/dice/lib/gradient_conatiner.dart
@@ -1,4 +1,3 @@
-import 'package:dice/styled_text.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
@@ -7,25 +6,39 @@ const endAlignment = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
 
+  // GradientContainer.purple() :
+  // color1 = Colors.deepPurple,
+  // color2 = Colors.indigo
+
   final Color color1;
   final Color color2;
+
+  void rollDice() {
+    //
+  }
 
   @override
   Widget build(context) {
     return Container(
-      decoration: BoxDecoration(
-        // 그라디언트
-        gradient: LinearGradient(
-          // colors: [Colors.red, Colors.blue],
-          colors: [color1, color2],
-          begin: startAlignment,
-          end: endAlignment,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            // colors: [Colors.red, Colors.blue],
+            colors: [color1, color2],
+            begin: startAlignment,
+            end: endAlignment,
+          ),
         ),
-      ),
-      // 디스플레이 중간에 추가해줌.
-      child: const Center(
-        child: StyledText("123"),
-      ),
-    );
+        child: Center(
+          child: Column(
+            // child: StyledText("123"),
+            children: [
+              Image.asset(
+                'assets/images/dice-2.png',
+                width: 200,
+              ),
+              TextButton(onPressed: rollDice, child: const Text('Roll Dice'))
+            ],
+          ),
+        ));
   }
 }
