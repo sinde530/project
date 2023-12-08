38db47ddc056a701e98bcb2672953fb8d5a5e972
Sinde530 <70146617+sinde530@users.noreply.github.com>
2023-12-09 00:43:01 +0900

    Add start_screen file

---
+++ flutter/dart_quiz/lib/start_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          // () {} 익명 함수
+          OutlinedButton(
+            onPressed: () {},
+            child: const Text('Start Quit'),
+          )
+        ],
+      ),
+    );
+  }
+}
