a4c66862b3c49b8a6f026c661ed281e1a7734310
Sinde530 <70146617+sinde530@users.noreply.github.com>
2024-01-25 16:28:30 +0900

    Write README.md

--- python/selenium-web-test-automation/page_object.py
+++ python/selenium-web-test-automation/page_object.py
@@ -27,3 +27,79 @@
 
 # -------------------------------- #
 
+# 리스트형 실습(2)
+# s = 'supercalifragilisticexpialidocious'
+# list_s = list(s)
+# print("list_s", list_s)
+# input_char = input("세어보기 원하는 알파벳 입력: ")
+
+# result = list_s.count(input_char)
+
+# print("입력된 문자 = %c 이고 몇 개가 들어있나? %d개" %(input_char, result))
+
+# -------------------------------- #
+
+# tuple
+# t = (12345, 54321, 'hello!')
+# print(t[2])
+
+# u = t, ("새로추가함", "1234")
+# print(u)
+
+# 집합
+# s = set("Python")
+# print(s)
+
+# t = set("AAA")
+# print(t)
+
+# -------------------------------- #
+
+# student_info = {
+#   "홍길동": 1234,
+#   "철수": 56789,
+#   "영희": 8272,
+# }
+
+# student_name = input("학생 이름을 입력하세요. ")
+
+# for d_name in student_info.keys():
+#   if student_name == d_name:
+#       student_number = student_info[d_name]
+#       print("이름: %s" %(student_name))
+#       print("학번: %s" %(student_number))
+#       break
+
+# -------------------------------- #
+
+# 파일 입출력
+# f = open('workfile', 'w')
+# f.write('abcd')
+# f.close() # save 역활
+
+# f = open("workfile", 'r')
+# print(f.read())
+
+# with open("python.txt", "w") as f:
+#   f.write("Life is too short, you need python")
+
+# 입력된 파일 불러와 구구단 출력
+# f = open("구구단.txt", "w")
+
+# for table in range(2, 10):
+#   table_name = "*** " + str(table) + "단" + " ***" + "\n"
+#   f.write(table_name)
+
+#   for number in range(1, 10):
+#     table_line = str(table) + '*' + str(number) + '=' + str(table * number) + "\n"
+#     f.write(table_line)
+# f.close()
+
+# filename = '구구단.txt'
+# f = open(filename, 'r')
+# lines = f.readlines()
+
+# for line in lines:
+#     print(line.strip())
+
+# -------------------------------- #
