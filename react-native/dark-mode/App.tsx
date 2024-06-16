acc5e146d3f73f519ac6f0f62fdeb65097149a42
Sinde530 <70146617+sinde530@users.noreply.github.com>
2024-06-16 21:01:37 +0900

    Add darkmode

--- react-native/dark-mode/App.tsx
+++ react-native/dark-mode/App.tsx
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, Pressable } from 'react-native';
+import { useColorScheme } from 'nativewind';
 
 export default function App() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+
   return (
-    <View className="m-auto w-full h-full justify-center items-center">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      className={`flex-1 justify-center items-center ${
+        colorScheme === 'dark' ? 'bg-black' : 'bg-white'
+      }`}
+    >
+      <Pressable
+        className="flex-1 items-center justify-center w-full h-full"
+        onPress={toggleColorScheme}
+      >
+        <Text className={`${colorScheme === 'dark' ? 'text-white' : 'text-dark'}`}>
+          {`Try clicking me! ${colorScheme} ${colorScheme === 'dark' ? '🌙' : '🌞'}`}
+        </Text>
+      </Pressable>
+
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
     </View>
   );
 }
