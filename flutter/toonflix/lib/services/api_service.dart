6eebdeef46d9d07424006cf066b1a074c3639ea8
Sinde530 <70146617+sinde530@users.noreply.github.com>
2024-12-19 17:55:55 +0900

    Add API

--- flutter/toonflix/lib/services/api_service.dart
+++ flutter/toonflix/lib/services/api_service.dart
@@ -1,8 +1,22 @@
+import 'package:http/http.dart' as http;
+
 class APiService {
   final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
   final String today = 'today';
 
-  void getTodaysToons() {
-    //
+  Future<void> getTodaysToons() async {
+    try {
+      final url = Uri.parse('$baseUrl/$today');
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        print(response.body);
+      } else {
+        print('Error : ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Failed to fetch data: $error');
+      throw Error();
+    }
   }
 }
